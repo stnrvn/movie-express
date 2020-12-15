3dea276fa7a05a1e6ae15318fb7cf1b5bf7a4d2a
sutanervin-dev <sutanervinlorenza@gmail.com>
2020-12-15 15:42:10 +0700

    part2 release:1

--- migrations/20201215081343-create-cast.js
+++ migrations/20201215081343-create-cast.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Casts', {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Casts', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -33,7 +33,7 @@ module.exports = {
       }
     });
   },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Casts');
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Casts');
   }
 };
